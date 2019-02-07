3ecf390d2b31a5daf3fc997b5d7e0a4ac826a957
Sam Saks-Fithian <samsaksfithian@gmail.com>
2019-02-07 10:59:53 -0800

    string functions

--- string.js
+++ string.js
@@ -21,7 +21,19 @@ const doggie2 = puppy.replace('o', 'labrador');
 console.log(doggie);
 console.log(doggie2);
 
+const color = puppy.slice(0, 6); // selects characters [0,6)
+const color4 = puppy.slice(7, -1);
+console.log(color);
+console.log(color4);
 
+const color2 = puppy.substring(0, 6); // alias of slice, does same thing
+console.log(color2);
+
+const color3 = puppy.substr(7, 5); // starts at a point and does a length, instead of start and end
+console.log(color3);
+
+const words = puppy.split(' ');
+console.log(words);
 
 
 
